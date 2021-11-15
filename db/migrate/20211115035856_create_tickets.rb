e3f6caace29aed50221ae9d5f32c80fce0970713
Masato Sugiyama <public@smasato.net>
2021-11-15 16:52:00 +0900

    add ticket model

---
+++ db/migrate/20211115035856_create_tickets.rb
@@ -0,0 +1,16 @@
+class CreateTickets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tickets do |t|
+      t.integer :item_id, null: false
+      t.string :url, null: false
+      t.string :status, null: false
+      t.datetime :issued_at, null: false
+      t.datetime :exchanged_at, null: true
+      t.datetime :disabled_at, null: true
+      t.references :line_user, foreign_key: true
+      t.references :coupon_setting, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
